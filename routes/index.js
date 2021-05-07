8b15118946decdaf5b9e7a5309f5bf9333240a81
EstherMS93 <esthermartinez@LAPTOP-BKUFCM5Q.localdomain>
2021-05-07 12:26:42 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,25 @@ router.get('/', (req, res, next) => res.render('index'));
 
 router.get("/movies", (req, res, next) => {
     movieModel.find()
-    .then((dbResult) => {
-        res.render('movies', {
-            movies: dbResult,
-        });
-    });
-});
+        .then((dbResult) => {
+            res.render('movies', {
+                movies: dbResult,
+            });
+        }).catch(error => next(error));
+ });
 
-module.exports = router;
+router.get("/movies/:id", (req, res, next) => {
+    movieModel.findById(req.params.id)
+        .then((dbResult) => {
+            res.render("details", {
+                oneMovie: dbResult,
+        });
+    }).catch(error => next(error));
+});
+router.get("/movies/delete/:id", (req, res, next) => {
+    movieModel.findByIdAndDelete(req.params.id)
+        .then((dbResult) => {
+            res.redirect("/movies");
+    }).catch(error => next(error));
+});
+    module.exports = router;
